5105a9d47f68dc51541895814854d50d64f50b98
Dynamic Duo <DynamicDuo@MacBook-Pro.local>
2018-05-24 14:46:25 +0800

    setup process.env.DATABASEURL to connect locally or with mLabs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://joshua:bucky123@ds133630.mlab.com:33630/todo_app' || 'mongodb://localhost:27017/TodoApp');
+
+//for changing to local db, on command line : export DATABASEURL=mongodb//localhost/yelp_camp_v12
+
+const url = process.env.DATABASEURL || 'mongodb://localhost:27017/TodoApp';
+
+//mongoose.connect('mongodb://joshua:bucky123@ds133630.mlab.com:33630/todo_app' || 'mongodb://localhost:27017/TodoApp');
+
+mongoose.connect(url);
 
 module.exports = { mongoose };
