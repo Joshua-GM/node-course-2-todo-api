cfc43a9d563bcee9a329eed79272b4443f2b3f5e
Dynamic Duo <DynamicDuo@MacBook-Pro.local>
2018-05-24 14:11:56 +0800

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://joshua:bucky123@ds133630.mlab.com:33630/todo_app' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };
