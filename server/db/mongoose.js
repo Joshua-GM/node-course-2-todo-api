1cbcbf2667bdec40e1e250b90e198ca569805d4d
Dynamic Duo <DynamicDuo@MacBook-Pro.local>
2018-05-28 22:42:50 +0800

    Setup separate test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 //for changing to local db, on command line : export DATABASEURL=mongodb//localhost/yelp_camp_v12
 
-const url = process.env.DATABASEURL || 'mongodb://localhost:27017/TodoApp';
+const url = process.env.DATABASEURL;
 
 //mongoose.connect('mongodb://joshua:bucky123@ds133630.mlab.com:33630/todo_app' || 'mongodb://localhost:27017/TodoApp');
 
